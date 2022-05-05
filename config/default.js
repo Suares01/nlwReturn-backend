4d012801b118cd0f450a5f620a0165fdadf4e33a
Suares01 <hub_suares.01@hotmail.com>
2022-05-05 17:51:22 -0300

    Add mailService config

--- config/default.js
+++ config/default.js
@@ -1,5 +1,15 @@
+require('dotenv/config');
+
+const { NODEMAILER_USER, NODEMAILER_PASS } = process.env;
+
 module.exports = {
   App: {
     port: 3333,
+    mailService: {
+      nodemailer: {
+        user: NODEMAILER_USER,
+        pass: NODEMAILER_PASS,
+      },
+    },
   },
 };
--- config/types.d.ts
+++ config/types.d.ts
@@ -1,3 +1,13 @@
-export interface IApp {
+export interface INodemailerConfig {
+  user: string;
+  pass: string;
+}
+
+export interface IMailServiceConfig {
+  nodemailer: INodemailerConfig;
+}
+
+export interface IAppConfig {
   port: number;
+  mailService: IMailServiceConfig;
 }
