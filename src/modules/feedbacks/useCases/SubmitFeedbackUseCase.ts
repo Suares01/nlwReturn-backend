7c92da96bece803931551e6f552d63ee3e7dac8c
Suares01 <hub_suares.01@hotmail.com>
2022-05-05 17:56:17 -0300

    Create SubmitFeedback use case

---
+++ src/modules/feedbacks/useCases/SubmitFeedbackUseCase.ts
@@ -0,0 +1,41 @@
+import { inject, injectable } from 'tsyringe';
+
+import { IFeedbacksRepository } from '@modules/feedbacks/repositories/IFeedbacksRepository';
+import { IMailService } from '@services/MailService';
+
+interface ISubmitFeedbackRequest {
+  type: string;
+  comment: string;
+  screenshot?: string;
+}
+
+@injectable()
+export class SubmitFeedbackUseCase {
+  constructor(
+    @inject('PrismaFeedbacksRepository')
+    private feedbacksRepository: IFeedbacksRepository,
+    @inject('NodemailerMailService')
+    private mailService: IMailService,
+  ) {}
+
+  public async execute({
+    comment,
+    type,
+    screenshot,
+  }: ISubmitFeedbackRequest): Promise<void> {
+    await this.feedbacksRepository.create({ comment, type, screenshot });
+
+    await this.mailService.sendMail({
+      subject: 'Novo Feedback',
+      body: [
+        `<div style="font-family: sans-serif; color: #222;">`,
+        `<h3>Feedback</h3>`,
+        '<div style="font-family: sans-serif; font-size: 16px; color: #111;">',
+        `<p>Tipo: ${type}</p>`,
+        `<p>Comentário: ${comment}</p>`,
+        '</div>',
+        `</div>`,
+      ].join('\n'),
+    });
+  }
+}
