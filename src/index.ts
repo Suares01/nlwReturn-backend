cc504d38ef406bdb815b1359d525b6d621bb5ecc
Suares01 <hub_suares.01@hotmail.com>
2022-05-06 22:02:46 -0300

    graceful start and graceful shutdown

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,35 @@
 import API from './Server';
 
-(() => {
-  API.prepareServer();
-  API.start();
+const exitSignals: NodeJS.Signals[] = ['SIGINT', 'SIGTERM', 'SIGQUIT'];
+
+process.on('unhandledRejection', (reason, promise) => {
+  console.error(
+    `App exiting due to an unhandled promise: ${reason} reason: ${promise}`,
+  );
+
+  throw reason;
+});
+
+process.on('uncaughtException', (error) => {
+  console.error(`App exiting due to an uncaught exception: ${error}`);
+
+  process.exit(1);
+});
+
+(async () => {
+  try {
+    await API.prepareServer();
+    API.start();
+
+    exitSignals.forEach((signal) => {
+      process.on(signal, async () => {
+        await API.close();
+        console.log(`App exited with success`);
+        process.exit(0);
+      });
+    });
+  } catch (error) {
+    console.error(`App exiting with error: ${error}`);
+    process.exit(1);
+  }
 })();
