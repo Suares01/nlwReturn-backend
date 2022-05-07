bb9750cdfce8be672d3904f14487d29dfb13238e
Suares01 <hub_suares.01@hotmail.com>
2022-05-07 18:42:25 -0300

    Change dev migrations to postgres

--- prisma/migrations/20220507213449_create_feedback_model/migration.sql
+++ prisma/migrations/20220507213449_create_feedback_model/migration.sql
@@ -1,7 +1,9 @@
 -- CreateTable
 CREATE TABLE "feedback" (
-    "id" TEXT NOT NULL PRIMARY KEY,
+    "id" TEXT NOT NULL,
     "type" TEXT NOT NULL,
     "comment" TEXT NOT NULL,
-    "screenshot" TEXT
+    "screenshot" TEXT,
+
+    CONSTRAINT "feedback_pkey" PRIMARY KEY ("id")
 );
